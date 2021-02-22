09f3b5d5c731f3018c5abb436165d0d8728ea7e0
Devinda Senanayaka <dca123@users.noreply.github.com>
2021-02-22 13:12:00 -0600

    added usernames

--- index.js
+++ index.js
@@ -7,14 +7,18 @@ const io = require('socket.io')(http)
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
-io.on('connection', (socket) => {
+io.on('connection', (socket, username) => {
+    socket.broadcast.emit('chat message', 'A user has connected !')
     console.log('a user connected');
     socket.on('disconnect', () => {
         console.log('a user disconnected');
     })
-    socket.on('chat message', (msg) => {
-        console.log(`message: ${msg}`);
-        io.emit('chat message', msg)
+    socket.on('chat message', (msg, username) => {
+        console.log(`username: ${msg}`);
+        io.emit('chat message', `${username}: ${msg}`)
+    })
+    socket.on('user connect', (username) => {
+        io.emit('chat message', `${username} has connected to the chatroom`)
     })
 
 })
