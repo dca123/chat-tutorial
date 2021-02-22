f47bbd6526a3a49509560e088e3354a5522d0903
Devinda Senanayaka <dca123@users.noreply.github.com>
2021-02-22 16:48:28 -0600

    support for heroku

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ io.on('connection', (socket) => {
     })
 
 })
-http.listen(3000, () => {
+http.listen(process.env.PORT || 3000, () => {
     console.log('Listening on http://localhost:3000');
 })
