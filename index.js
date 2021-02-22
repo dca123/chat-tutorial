9effe18bae051968e636645cec3190dc5e567b48
Devinda Senanayaka <dca123@users.noreply.github.com>
2021-02-22 14:54:10 -0600

    adding message now broadcasts and not emits

--- index.js
+++ index.js
@@ -8,14 +8,11 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 io.on('connection', (socket, username) => {
-    socket.broadcast.emit('chat message', 'A user has connected !')
-    console.log('a user connected');
     socket.on('disconnect', () => {
-        console.log('a user disconnected');
+        io.emit('chat message', `${username} has left to the chatroom.`)
     })
     socket.on('chat message', (msg, username) => {
-        console.log(`username: ${msg}`);
-        io.emit('chat message', `${username}: ${msg}`)
+        socket.broadcast.emit('chat message', `${username}: ${msg}`)
     })
     socket.on('user connect', (username) => {
         io.emit('chat message', `${username} has connected to the chatroom.`)
