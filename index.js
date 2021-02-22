e92a73a03d3ca4ffcd793668257f443e41739f31
Devinda Senanayaka <dca123@users.noreply.github.com>
2021-02-22 15:56:05 -0600

    added active users and disconnection

--- index.js
+++ index.js
@@ -7,15 +7,22 @@ const io = require('socket.io')(http)
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
+const liveUsers = []
 io.on('connection', (socket) => {
-    socket.on('disconnect', (username) => {
-        io.emit('chat message', `${username} has left to the chatroom.`)
+    let stateUsername = ""
+    socket.on('disconnect', () => {
+        io.emit('chat message', `${stateUsername} has left to the chatroom.`)
+        liveUsers.splice(liveUsers.indexOf(stateUsername), 1);
+        io.emit('user connect', liveUsers)
     })
     socket.on('chat message', (msg, username) => {
         socket.broadcast.emit('chat message', `${username}: ${msg}`)
     })
     socket.on('user connect', (username) => {
         io.emit('chat message', `${username} has connected to the chatroom.`)
+        liveUsers.push(username)
+        io.emit('user connect', liveUsers)
+        stateUsername = username;
     })
     socket.on('chat input', (username) => {
         socket.broadcast.emit('chat input', `${username} is typing ...`)
